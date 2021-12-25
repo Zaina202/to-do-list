099a9482b40215af8d4c697c1f834c734bf29678
Karam47 <s12028942@stu.najah.edu>
2021-12-25 16:08:37 +0200

    problems solved
    a problems happened while members uploading their tasks
    we have always a backup

--- web2.js
+++ web2.js
@@ -48,18 +48,18 @@
             "United states", "New Zealand", "United Kingdom"]
     }
 function init(){
-    let mydiv = qs('.jobContainer');
-    for (let i = 0; i < 12; i++) {
-        let div1 = `<div class = "job">
+    let mydiv=qs('.jobContainer');
+    for(let i = 0;i<12;i++){
+       let div1 = `<div class = "job">
                    <a href = "index.html"><img src = "${object.imageArray[i]}" alt = "Error image" class = "imageAlt"></a>
                    <p class = "p1">${object.timeArray[i]}</p>
                    <p class = "p2"><a href = "index.html">${object.jobArray[i]}</a></p>
                    <p class = "p1">${object.companyArray[i]}</p>
                    <p class = "p3">${object.areaArray[i]}</p>
                    </div>`;
-        mydiv.innerHTML += div1;
+       mydiv.innerHTML += div1;
     }
-    for (let i = 12; i < 24; i++) {
+    for(let i=12;i<24;i++){
         let div2 = `<div class = "job1">
         <a href = "index.html"><img src = "${object.imageArray[i]}" alt = "Error image" class = "imageAlt"></a>
                    <p class = "p1">${object.timeArray[i]}</p>
@@ -67,7 +67,7 @@ function init(){
                    <p class = "p1">${object.companyArray[i]}</p>
                    <p class = "p3">${object.areaArray[i]}</p>
                    </div>`;
-        mydiv.innerHTML += div2;
+            mydiv.innerHTML += div2;
     }
     let myBtn = `<div id = "btnDiv"><button id = "btnL" class= "button" >Load More</button></div>`;
     mydiv.innerHTML+=myBtn;
